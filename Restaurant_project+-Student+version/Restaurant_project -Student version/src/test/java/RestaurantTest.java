fdacf06421eb397541fc46ecb7cb93c5dfbf78eb
aksharadammur <aksharadammur@gmail.com>
2023-04-12 00:34:10 +0530

    Part 3: Failing test case

--- Restaurant_project+-Student+version/Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -83,5 +83,21 @@ class RestaurantTest {
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
-    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void TotalAmountOf_Selected_Menu_items(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Patsa Alfredo",120);
+
+        List<String> selectedItem=new ArrayList<>();
+        selectedItem.add("Sweet corn soup");
+        selectedItem.add("Vegetable lasagne");
+        int actual=restaurant.getOrderTotal(selectedItem);
+        assertEquals(388,actual);
+
+
+    }//<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
